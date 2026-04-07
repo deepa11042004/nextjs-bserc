862c2beefc7c20cced37134691d57ec9abb12fec
Rohit Chauhan <nixenboi@gmail.com>
2026-04-06 22:58:12 -0700

    works

--- src/app/(internships)/bsercinternship/page.tsx
+++ src/app/(internships)/bsercinternship/page.tsx
@@ -1,21 +1,21 @@
-import { TimelineSection } from "@/components/InternshipSection/KeyDate";
+// import { TimelineSection } from "@/components/InternshipSection/KeyDate";
  
 
-import Paragraphs from "@/components/InternshipSection/Paragraphs";
-import { Services } from "@/components/InternshipSection/Services";
-import StatsCard from "@/components/InternshipSection/StatsCard";
-import ProgramBenefits from "@/components/InternshipSection/ProgramBenefits";
-import ProgramOverview from "@/components/InternshipSection/ProgramOverview";
-import ResultsAnnouncements from "@/components/InternshipSection/ResultsAnnouncements";
-import { CompleteTimeline } from "@/components/InternshipSection/CompletionTimeline";
-import Hero from "@/components/InternshipSection/Hero";
-// import HeroSlider from "@/components/InternshipSection/HeroSlider";
+// import Paragraphs from "@/components/InternshipSection/Paragraphs";
+// import { Services } from "@/components/InternshipSection/Services";
+// import StatsCard from "@/components/InternshipSection/StatsCard";
+// import ProgramBenefits from "@/components/InternshipSection/ProgramBenefits";
+// import ProgramOverview from "@/components/InternshipSection/ProgramOverview";
+// import ResultsAnnouncements from "@/components/InternshipSection/ResultsAnnouncements";
+// import { CompleteTimeline } from "@/components/InternshipSection/CompletionTimeline";
+// import Hero from "@/components/InternshipSection/Hero";
+import HeroSlider from "@/components/InternshipSection/HeroSlider";
 
 const page = () => {
   return (
     <div className="bg-black">
-      {/* <HeroSlider /> */}
-      <Hero/>
+     <HeroSlider />
+      {/* <Hero/>
       <Paragraphs />
       <StatsCard />
       <Services />
@@ -23,7 +23,7 @@ const page = () => {
       <CompleteTimeline />
       <ProgramOverview />
       <ResultsAnnouncements />
-      <ProgramBenefits />
+      <ProgramBenefits /> */}
     </div>
   );
 };
